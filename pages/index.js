8d86145c6a9387882aa2d200547680d5eee51265
its22013 <s22013@std-college.ac.jp>
2023-03-07 06:35:28 +0000

    googlebook

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import styles from '../styles/search.module.css'
+import Image from 'next/image'
 
 function Home () {
   const [query, setQuery] = useState('')
@@ -12,25 +13,39 @@ function Home () {
     const data = await res.json()
     setBooks(data.items || [])
   }
-
   return (
     <div>
-      <div className={styles.container}>
-        <h1>Google Books API Search</h1>
-        <h2>by(shuya)</h2>
+      <div className={styles.background}>
+        <div className={styles.container}>
+          <h1>本を検索するAPI</h1>
+          <h2>by(shuya)</h2>
+        </div>
       </div>
-      <div className={styles.container}>
-        <input type='text' value={query} onChange={(e) => setQuery(e.target.value)} />
-        <button onClick={searchBooks}>Search</button>
+      <div className={styles.searchform}>
+        <div className={styles.container}>
+          <input type='text' value={query} onChange={(e) => setQuery(e.target.value)} />
+          <button onClick={searchBooks}>Search</button>
+        </div>
       </div>
       {books.map((book) => (
         <div key={book.id}>
-          <h2>{book.volumeInfo.title}</h2>
-          <p>{book.volumeInfo.authors?.join(', ')}</p>
+          <div className={styles.container}>
+            <h2>{book.volumeInfo.title}</h2>
+            <p>{book.volumeInfo.authors?.join(', ')}</p>
+          </div>
           <p>{book.volumeInfo.description}</p>
           <img src={book.volumeInfo.imageLinks?.thumbnail} alt='' />
         </div>
       ))}
+      <div>
+        <Image
+          src='/images/lakaeslogo.jpg'
+          alt='My Image3'
+          layout='responsive'
+          width={100} height={100}
+          style={{ display: 'block', margin: 'auto' }}
+        />
+      </div>
     </div>
   )
 }
